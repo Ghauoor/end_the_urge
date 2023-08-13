8a8ad314f4e08eada8a7371ab9c5732ad961bc30
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-13 23:58:59 +0500

    End the Urge

---
+++ App.js
@@ -0,0 +1,38 @@
+import {
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Alert,
+  View,
+  Dimensions,
+} from 'react-native';
+import React from 'react';
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from '@react-native-community/hooks';
+
+const App = () => {
+  console.log(useDimensions());
+  console.log(useDeviceOrientation());
+  const {landscape} = useDeviceOrientation();
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: '100%',
+          height: landscape ? '100%' : '30%',
+        }}></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;

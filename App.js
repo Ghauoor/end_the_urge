ca6a4f437914464f0de5100a11f6dc4711716f24
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-14 18:44:32 +0500

    Text Properties

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import WelcomScreen from './src/screens/WelcomScreen';
-import ViewImageScreen from './src/screens/ViewImageScreen';
+import {View, StyleSheet, Text} from 'react-native';
 
 const App = () => {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 30,
+          fontStyle: 'italic',
+          color: 'tomato',
+          fontWeight: '600',
+          textTransform: 'capitalize',
+          textAlign: 'center',
+          lineHeight: 30,
+        }}>
+        I love react native, This is the first time i contact with the React
+        Native.
+      </Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({});
 
 export default App;
